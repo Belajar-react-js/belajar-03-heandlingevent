589a018244b96c4ee138b3f6fa21ed33d4cadc7e
kharisma tara mukti <kharismataram@gmail.com>
2023-05-10 21:47:59 +0700

    counter

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+
+function Counter() {
+  return (
+    <>
+      <Button>-</Button>
+      <span>0</span>
+      <Button>+</Button>
+    </>
+  );
+}
+
+export default Counter;
